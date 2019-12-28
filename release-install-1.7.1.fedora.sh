f9d8e8cec26dfe0bcf51eb93e747c478adeb2936
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-12-28 17:05:37 -0500

    Update release-install-1.7.1.fedora.sh

--- release-install-1.7.1.fedora.sh
+++ release-install-1.7.1.fedora.sh
@@ -1,3 +1,4 @@
 wget https://github.com/c4pt000/openvisualtraceroute/releases/download/deb/ovtr_1.7.1-1_amd64.deb
+yum install dpkg alien -y
 sudo dpkg -x ovtr_1.7.1-1_amd64.deb /
 /usr/share/OpenVisualTraceRoute/gksu_ovtr.sh
